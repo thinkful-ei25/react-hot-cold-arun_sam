081515f9269f6c7b3d3c55ec2402d9ae3873a90b
Sam Dalton <spd4nf@virginia.edu>
2018-11-08 10:59:07 -0600

    added state, added feedback

--- src/components/game.js
+++ src/components/game.js
@@ -5,14 +5,56 @@ import GuessSection from './guess-section';
 import GuessCount  from './guess-count';
 import GuessList from './guess-list';
 
-export default function Game(props) {
-    return (
+export default class Game extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            count: 0,
+            guesses : [],
+            solution: 50,
+            lastGuess: null,
+        };
+    }
+
+    handleNewGuess(val){
+        this.setState({
+            count: this.state.count+1,
+            guesses: [...this.state.guesses, val]
+        });
+    }
+
+    computeFeedback(val){
+        let difference = Math.abs(val-this.state.solution);
+        if(difference >= 50){
+            return 'cold'
+        } else if (difference >= 25){
+            return 'lukewarm'
+        } else if (difference >=10){
+            return 'warm'
+        } else if (difference >=5){
+            return 'hot'
+        } else if (difference >0){
+            return 'super hot'
+        } else if (difference === 0){
+            return 'You win!';
+        }
+        return 'Enter your guess!';
+        
+
+       
+    }
+    
+    render() { 
+        
+        const feedback = this.computeFeedback(this.state.guesses[this.state.count-1]);
+
+        return (
         <div>
             <Header />
-            <GuessSection feedback="Make your guess!" />
-            <GuessCount count={3} />
-            <GuessList guesses={[10, 15, 25]} />
+            <GuessSection feedback={feedback} onSubmit={guess=>this.handleNewGuess(guess)}/>
+            <GuessCount count={this.state.count} />
+            <GuessList guesses={this.state.guesses} />
         </div>
-    );
+    )};
 }
 
